a96b3291248b90deec5c3907bd03f57cabff2c6b
Kunal Sharma <kunal.s@technover.com>
2021-09-02 13:34:30 +0530

    Added sign up fields.

--- client/components/signup.js
+++ client/components/signup.js
@@ -9,8 +9,11 @@ import { useMutation } from '@apollo/client';
 import { CREATE_USERS } from '../GraphQL/Mutations/UsersMutation';
 import { useRouter } from 'next/router'
 import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     button: {
         margin: "20px",
         backgroundColor: "#0596f5",
@@ -18,8 +21,13 @@ const useStyles = makeStyles({
         padding: "20px",
         borderRadius: "40px",
         textAlign: "center"
-    }
-});
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 220,
+    },
+}));
 
 export default function Signup() {
     const classes = useStyles();
@@ -40,8 +48,17 @@ export default function Signup() {
     const [fullName, setFullName] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
     const [gstNumber, setGstNumber] = useState("");
-    const [location, setLocation] = useState("");
     const [storeName, setStoreName] = useState("");
+    const [rating, setRating] = useState(0);
+    const [area, setArea] = useState("");
+    const [landmark, setLandmark] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [country, setCountry] = useState("");
+    const [openTime, setOpenTime] = useState("07:30");
+    const [closeTime, setCloseTime] = useState("20:30");
+    const [statusTypes, setStatusTypes] = useState(["Open", "Closed"]);
+    const [status, setStatus] = useState("Open");
     const [createUser] = useMutation(CREATE_USERS);
     const router = useRouter();
 
@@ -88,13 +105,69 @@ export default function Signup() {
                     <br /><br />
                     <TextField label="Full Name" variant="outlined" color="primary" value={fullName} onChange={(e) => { setFullName(e.target.value); }} />
                     <br /><br />
-                    <TextField label="Store Name" variant="outlined" color="primary" value={storeName} onChange={(e) => { setStoreName(e.target.value); }} />
-                    <br /><br />
-                    <TextField label="Location" variant="outlined" color="primary" value={location} onChange={(e) => { setLocation(e.target.value); }} />
-                    <br /><br />
                     <TextField label="Phone Number" variant="outlined" color="primary" value={phoneNumber} onChange={(e) => { setPhoneNumber(e.target.value); }} />
                     <br /><br />
                     <TextField label="GST Number" variant="outlined" color="primary" value={gstNumber} onChange={(e) => { setGstNumber(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="Store Name" variant="outlined" color="primary" value={storeName} onChange={(e) => { setStoreName(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="Rating" variant="outlined" color="primary" value={rating} onChange={(e) => { setRating(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="Area" variant="outlined" color="primary" value={area} onChange={(e) => { setArea(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="Landmark" variant="outlined" color="primary" value={landmark} onChange={(e) => { setLandmark(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="City" variant="outlined" color="primary" value={city} onChange={(e) => { setCity(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="State" variant="outlined" color="primary" value={state} onChange={(e) => { setState(e.target.value); }} />
+                    <br /><br />
+                    <TextField label="Country" variant="outlined" color="primary" value={country} onChange={(e) => { setCountry(e.target.value); }} />
+                    <br /><br />
+                    <TextField
+                        id="openTime"
+                        label="Open Time"
+                        type="time"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        inputProps={{
+                            step: 300, // 5 min
+                        }}
+                        value={openTime}
+                        onChange={(e) => { setOpenTime(e.target.value); }}
+                    />
+                    <br /><br />
+                    <TextField
+                        id="closeTime"
+                        label="Close Time"
+                        type="time"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        inputProps={{
+                            step: 300, // 5 min
+                        }}
+                        value={closeTime}
+                        onChange={(e) => { setCloseTime(e.target.value); }}
+                    />
+                    <br /><br />
+                    <FormControl variant="outlined" className={classes.textField} required autoComplete="status" autoFocus>
+                        <InputLabel htmlFor="category">Status</InputLabel>
+                        <Select
+                            native
+                            name="status"
+                            value={status}
+                            onChange={(e) => { setStatus(e.target.value); }}
+                            label="Status"
+                        >
+                            <option aria-label="None" value="" />
+                            {statusTypes.map((status, index) =>
+                                <option key={index} value={status}>{status}</option>
+                            )}
+                        </Select>
+                    </FormControl>
                     <br /><br />
                     <Button type="submit" variant="contained" color="secondary" className={classes.button}>Register</Button> <br />
 
